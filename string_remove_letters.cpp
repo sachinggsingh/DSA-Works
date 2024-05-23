00ba0ab5370217ab933011ecc8af0aeaf7bff6b7
Sachin Singh <152975718+sachinggsingh@users.noreply.github.com>
2024-05-23 22:16:13 +0530

    Update string_remove_letters.cpp

--- string_remove_letters.cpp
+++ string_remove_letters.cpp
@@ -6,9 +6,10 @@ using namespace std;
 int main() {
     string A = "Enthusiast";
     int j = 0;
-    for (int i = 0; i < A.size(); i++) {
-        if (A[i] != 'a' && A[i] != 'e' && A[i] != 'i' && A[i] != 'o' && A[i] != 'u' &&
-            A[i] != 'A' && A[i] != 'E' && A[i] != 'I' && A[i] != 'O' && A[i] != 'U') {
+    for (int i = 0; i < A.size(); i++) 
+    {
+        if (A[i] != 'a' && A[i] != 'e' && A[i] != 'i' && A[i] != 'o' && A[i] != 'u' && A[i] != 'A' && A[i] != 'E' && A[i] != 'I' && A[i] != 'O' && A[i] != 'U')
+        {
             A[j++] = A[i];
         }
     }
